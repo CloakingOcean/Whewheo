f5c4760f169eb3b0f96b3b310033f43cdd3b9c34
Shawn <rocketrobot3@gmail.com>
2017-05-15 15:33:16 -0600

    Removed debug code causing error
    Removed debug code that tries to make a new file in the c/temp folder.

--- src/com/exitium/whewheo/Main.java
+++ src/com/exitium/whewheo/Main.java
@@ -148,26 +148,6 @@ public class Main extends JavaPlugin implements PluginMessageListener{
 		sentPlayersFile = new File(config.getString("general.bungeecordFolder"), "sentplayers.yml");
 		Bukkit.getServer().getLogger().info("Sent Players File Path: " + sentPlayersFile.getPath());
 		
-		//Attempting to save a normal text file 
-		
-		File file = new File("c://temp//testFile1.txt");
-		
-		try {
-			//Create the file
-			if (file.createNewFile()){
-				System.out.println("File is created!");
-			}else{
-				System.out.println("File already exists.");
-			}
-			 
-			//Write Content
-			FileWriter writer = new FileWriter(file);
-			writer.write("Test data");
-			writer.close();
-		}catch (Exception e) {
-			e.printStackTrace();
-		}
-		
 //
 //		if (!sentPlayersFile.exists()) {
 //			saveResource("sentplayers.yml", false);
